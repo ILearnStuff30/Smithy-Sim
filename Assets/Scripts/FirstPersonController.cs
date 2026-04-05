31d700e596d44752d5117f50799488e83e5d7284
ILearnStuff30 <LOUIS.MARSHALL24@bathspa.ac.uk>
2026-04-05 17:05:19 +0100

    Update: Added drag and jump
    Added drag and jump movement functions to FirstPersonController script

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -6,11 +6,22 @@ public class FirstPersonController : MonoBehaviour
 {
     public Rigidbody rb;
     public float speed = 5f;
+    public float jumpPower = 5f;
+
+ 
+    public float playerDrag;
+
+    public float playerHeight;
+    public LayerMask groundLayer;
+    public bool isGrounded;
+
+    public bool canJump;
 
     public float horizontalInput;
     public float verticalInput;
 
     Vector3 moveDirection;
+    Vector3 jumpDirection;
 
     public Transform playerTransform;
 
@@ -23,10 +34,25 @@ public class FirstPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundLayer);
 
         // Gets the vertical and horizontal input from the Input Manager
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+
+        if(isGrounded)
+        {
+            rb.drag = playerDrag;
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+            rb.drag = 0;
+        }
+
+        SpeedControl();
+        JumpCheck();
     }
 
     private void FixedUpdate()
@@ -41,4 +67,27 @@ public class FirstPersonController : MonoBehaviour
 
         rb.AddForce(moveDirection.normalized * speed * 10f, ForceMode.Force);
     }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        
+        if(flatVelocity.magnitude > speed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * speed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
+    }
+
+    private void JumpCheck()
+    {
+        jumpDirection = transform.up * jumpPower;
+
+        if(isGrounded && canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(jumpDirection.normalized * jumpPower * 5f, ForceMode.Impulse);
+            canJump = false;
+        }
+    }
+    
 }
