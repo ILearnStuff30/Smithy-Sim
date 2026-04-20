a4e3a63520bda9ba1e9ad282287b443181a598d1
ILearnStuff30 <LOUIS.MARSHALL24@bathspa.ac.uk>
2026-04-20 17:59:22 +0100

    Made forge functionality
    Made forge functionality to work with the metal script. Made minor changes to camera scripts but still needs to be worked on next time.

--- Assets/Scripts/ForgeScript.cs
+++ Assets/Scripts/ForgeScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,21 +8,29 @@ public class ForgeScript : MonoBehaviour
 {
     public float rateOfHeatChange;
 
+    public float forgeTime;
+
     private metalScript metalScript;
 
-    GameObject materialGameobject;
-
-    private void Awake()
-    {
-        metalScript = materialGameobject.GetComponent<metalScript>();
-    }
 
     public void Update()
     {
-        if(this.gameObject.GetComponentInChildren<metalScript>() != null)
+        metalScript = this.gameObject.GetComponentInChildren<metalScript>();
+        if (this.gameObject.GetComponentInChildren<metalScript>() != null)
         {
             Debug.Log("Heating up");
-            metalScript.heat += rateOfHeatChange + Time.deltaTime;
+            forgeTime += Time.deltaTime;
+
+            metalScript.heat = Mathf.Pow(Mathf.Exp(1), forgeTime / 15);
+            if (metalScript.heat > 50)
+            {
+                metalScript.stress += Mathf.Pow(Mathf.Exp(1), (metalScript.heat - 70) / 13) * Time.deltaTime;
+            }
+
+            Debug.Log("The metal's heat is "+ metalScript.heat);
+            // Forging temperature for steel is 1230 degrees celcius
         }
+
+
     }
 }
--- Assets/Scripts/metalScript.cs
+++ Assets/Scripts/metalScript.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 public class metalScript : MonoBehaviour
 {
-    public float heat = 0;
+
+    // Measured in degrees celcius. Room temperature is 20-22 degrees Celcius.
+    public float heat = 21;
     public float stress = 0;
 
     public void changeHeat(int heatChange)
