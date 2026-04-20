8dba8d3aa9d0196b2e24d155f3c569f91e5620df
Prismatic-Echo <alx75730@gmail.com>
2026-04-20 13:25:21 +0100

    fix: Improved metal interactions
    Made it so that metal interactions work with every workstatation tag it gets given, with this being flexable to give more or less valid tags.

--- Assets/Scripts/Material Interaction.cs
+++ Assets/Scripts/Material Interaction.cs
@@ -1,96 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
-    // Declare the scripts for the workstations to access their "materialGameobject" variable
-    public ForgeScript forgeScript;
-    public AnvilScript anvilScript;
-    public WorkbenchScript workbenchScript;
-    public GameObject materialContainer;
-
-    private bool hasMaterial, disablePickup = false;
-    public bool interactable = true;
-
     public GameManager gameManager;
 
-    //public void giveWorkstationMaterial(GameObject materialGameObjectForOverlay, GameObject workstationMaterialContainer)
-    //{
-    //    Debug.Log(workstationMaterialContainer.gameObject.name);
-    //    if (workstationMaterialContainer = null)
-    //    {
-    //        Debug.Log("Dropped off");
-    //        workstationMaterialContainer = cloneGameOjbect(materialGameObjectForOverlay);
-    //        hasMaterial = false;
-    //    } else
-    //    {
-    //        Debug.Log("Picked up");
-    //        materialGameObjectForOverlay = cloneGameOjbect(workstationMaterialContainer);
-    //        workstationMaterialContainer = null;
-    //        hasMaterial = true;
-    //    }
-    //}
+    private string[] workstationTags = { "forge", "anvil", "workbench" };
+
+    public void giveWorkstationMaterial(Collider collider)
+    {
+        if (gameManager.objectContainingMetal == this.gameObject)
+        {
+
+            gameManager.changeMetalHolder(collider.gameObject, collider.transform.position, new Vector3(collider.transform.rotation.x, collider.transform.rotation.y, collider.transform.rotation.z));
+
+            Debug.Log("Metal given to |" + collider.tag + "| from player");
+
+        }
+        else if (gameManager.objectContainingMetal == GameObject.FindGameObjectsWithTag(collider.tag)[0])
+        // FindGameObjectWithTag returns an array, but were only ever expecting 1 result
+        {
+
+            gameManager.changeMetalHolder(this.gameObject, Vector3.zero, -Vector3.forward);
+
+            Debug.Log("Metal given to player from |" + collider.tag + "|");
+
+        }
+    }
 
     // if the player is in range of a colldier
     private void OnTriggerStay(Collider collider)
     {
 
-        // if the tag of whatever we interacted with is the material, is interactable and pickup hasnt been disabled
-        if (collider.tag == "material" && Input.GetKey(KeyCode.E) && !disablePickup && interactable) 
+        // if the tag of whatever we interacted with is the material
+        if (collider.tag == "material" && Input.GetKeyDown(KeyCode.E)) 
         {
             gameManager.changeMetalHolder(this.gameObject, Vector3.zero, -Vector3.forward); // This is wrong!!
             
-            Debug.Log("Metal given to player");
+            Debug.Log("Metal given to player from world");
         }
 
-        // if the tag of whatever we interacted with is the forge
-        if (collider.tag == "forge" && Input.GetKey(KeyCode.E) /*&& interactable*/) 
+        if (workstationTags.Contains<String>(collider.tag))
         {
-            gameManager.changeMetalHolder(collider.gameObject, collider.transform.position, new Vector3(collider.transform.rotation.x, collider.transform.rotation.y, collider.transform.rotation.z));
-            interactable = false;
-
-            Debug.Log("Metal given to forge");
+            giveWorkstationMaterial(collider);
         }
-
-        // if the tag of whatever we interacted with is the anvil
-        if (collider.tag == "anvil" && Input.GetKey(KeyCode.E) && interactable) 
-        {
-            //giveWorkstationMaterial(materialContainer, anvilScript.materialGameobject);
-            interactable = false;
-        }
-
-        // if the forge has a material and is interacted with
-        //if (collider.tag == "anvil" && Input.GetMouseButtonDown(1) && forgeScript.materialGameobject != null && interactable)
-        //{
-        //    interactable = false;
-        //}
-
-        // if the tag of whatever we interacted with is the workbench
-        if (collider.tag == "workbench" && Input.GetKey(KeyCode.E) && interactable) 
-        {
-            //giveWorkstationMaterial(materialContainer, workbenchScript.materialGameobject);
-            interactable = false;
-        }
-
-        if (Input.GetKeyUp(KeyCode.E))
-        {
-            interactable = true;
-        }
-    }
-
-    public void Update()
-    {
-
-        if (materialContainer != null)
-        {
-            // If the player has the metal, make it appear in the overlay
-            materialContainer.transform.SetParent(transform, false);
-            materialContainer.transform.position = Vector3.zero;
-        }
-
     }
 }
