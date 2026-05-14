dc6898bb91996affa732c7810191563767c4b1d6
ILearnStuff30 <LOUIS.MARSHALL24@bathspa.ac.uk>
2026-05-14 19:52:07 +0100

    Test Materials with Feel

---
+++ Assets/Scripts/TestScripts/FeelMaterialTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Feedbacks;
+
+public class FeelMaterialTest : MonoBehaviour
+{
+    public MMF_Player materialFeedback;
+
+    public bool isInterpolating;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isInterpolating)
+        {
+            materialFeedback?.PauseFeedbacks();
+        }
+        else
+        {
+            materialFeedback?.ResumeFeedbacks();
+        }
+    }
+}
